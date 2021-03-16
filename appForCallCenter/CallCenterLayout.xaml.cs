32d4b618a72e18ba6dba3eb9bfcf34eb1094e70f
tohirjanakhrarov <tohirjanakhrarov@gmail.com>
2021-03-17 02:34:13 +0500

    Добавьте файлы проекта.

---
+++ appForCallCenter/CallCenterLayout.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Npgsql;
+using Newtonsoft.Json.Linq;
+
+namespace appForCallCenter
+{
+    /// <summary>
+    /// Логика взаимодействия для CallCenterLayout.xaml
+    /// </summary>
+    public partial class CallCenterLayout : Window
+    {
+        public CallCenterLayout()
+        {
+            InitializeComponent();
+        }
+        public string thisUserLogin;
+
+
+
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (telNumberInpt.Text.Length == 9 && tinInpt.Text.Length == 9 && problemInpt.Text.Length > 0)
+            {
+                DateTime dateTime = DateTime.Today;
+                string currentDate = dateTime.ToString("D");
+                string clientTin = tinInpt.Text;
+                string clientTelNumber = telNumberInpt.Text;
+                string problemContent = problemInpt.Text;
+                MainWindow mainWindow = new MainWindow();
+               /* thisUserLogin = mainWindow.userLogin;*/
+
+
+                string connString = "Host=192.168.70.114;Port=7777;Database=postgres;Username=postgres;Password=13123866rT;";
+                NpgsqlConnection connection = new NpgsqlConnection(connString);
+                connection.Open();  
+                NpgsqlCommand insertClientProblems = new NpgsqlCommand("INSERT INTO mybuxuchet.user_problems(added_user, client_tin, client_number, problem_content, added_date) VALUES ('"+thisUserLogin+ "','"+clientTin+"','"+clientTelNumber+"','"+problemContent+"','"+currentDate+"');", connection);
+                insertClientProblems.ExecuteNonQuery();
+                connection.Close();
+                /*errorsLabel.Content = currentDate;*/
+                tinInpt.Clear();
+                telNumberInpt.Clear();
+                problemInpt.Clear();
+            }
+            else
+            {
+                errorsLabel.Content = "nimadur to'liq emas";
+            }
+        }
+    }
+}
---
+++ appForCallCenter/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Npgsql;
+using Newtonsoft.Json.Linq;
+
+namespace appForCallCenter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+             
+           
+        }
+
+        public void button_Click(object sender, RoutedEventArgs e)
+        {   
+            string userLogin = loginInpt.Text;
+            string userPass = passwordBox.Password;
+            string connString = "Host=192.168.70.114;Port=7777;Database=postgres;Username=postgres;Password=13123866rT;";
+            NpgsqlConnection connection = new NpgsqlConnection(connString);
+            connection.Open();
+            var selectUser = new NpgsqlCommand("SELECT user_role FROM mybuxuchet.users_list where user_login ='"+userLogin+"' and user_pass ='"+userPass+"';", connection);
+            /*if (selectUser.AllResultTypesAreUnknown == false)
+            {
+                errorsLabel.Content = "user topilmadi";
+            }*/
+            /*else { */
+
+            var result = selectUser.ExecuteScalar();
+            if (result == null)
+            {
+                errorsLabel.Content = "user Topilmadi!";
+            }
+
+            else
+            {
+                int roleid = Convert.ToInt32(result);
+                if (roleid == 7)
+                {
+                    errorsLabel.Content = "is Admin";
+                   
+                }
+
+                else if (roleid == 6)
+                {
+                    errorsLabel.Content = "is Users";
+                    CallCenterLayout callCenterLayout = new CallCenterLayout();
+                    callCenterLayout.Show();
+                    callCenterLayout.thisUserLogin = userLogin;
+                }
+
+            }
+             
+        }
+        
+
+
+    }   
+    }
+
